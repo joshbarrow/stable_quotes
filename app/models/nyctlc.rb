7c95e0ac46bbb2d4da50cde11c38e2ff362fac31
Josh <joshrbarrow@gmail.com>
2020-06-26 15:26:17 -0700

    refactor license lookup to nyctlc

--- app/models/nyctlc.rb
+++ app/models/nyctlc.rb
@@ -1,11 +1,17 @@
-class LicenseLookup
+class Nyctlc
   def initialize(tlc:)
     @tlc = tlc
   end
 
-  def driving_more_than_3_years?
+  def licensed_at_least_3_years?
+    num_years_licensed >= 3
+  end
+
+  def num_years_licensed
     # here we are going to calculate randomly, but IRL we
     # would be integrating with a 3rd party service
-    !(@tlc =~ /[374]/).nil?
+    !(@tlc =~ /[374]/).nil? ?
+      4 :
+      2
   end
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,19 +5,17 @@ class Quote < ApplicationRecord
   SAFE_DRIVER_RATE = 1500
 
   def price
-    at_risk_driver = driving_more_than_3_years?
-
-    at_risk_driver ?
+    at_risk_driver? ?
       AT_RISK_DRIVER_RATE :
       SAFE_DRIVER_RATE
   end
 
-  def license_lookup
-    LicenseLookup.new(tlc: data.fetch("tlc"))
-  end
-
   private
 
-  delegate :driving_more_than_3_years?, to: :license_lookup
+  def license_lookup
+    Nyctlc.new(tlc: data.fetch("tlc"))
+  end
 
+  delegate :licensed_at_least_3_years?, to: :license_lookup
+  alias_method :at_risk_driver?, :licensed_at_least_3_years?
 end
